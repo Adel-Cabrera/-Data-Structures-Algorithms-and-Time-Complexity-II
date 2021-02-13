ea57c3004134badd665a2aa7a13f73c52a9bb6a3
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2021-02-12 22:51:32 -0300

    adding some more bitchass code

--- src/main/java/algorithms/BigO.java
+++ src/main/java/algorithms/BigO.java
@@ -3,7 +3,8 @@ package algorithms;
 public class BigO {
 
 
-    public void foo(int[] array) {
+//	O(N)
+    public void foo(int[] array) { 
 
         for (int i = 0; i < array.length; i++) {
             // ...
@@ -15,6 +16,7 @@ public class BigO {
 
     }
 
+//    O(N * M)
     public void bar(int[] array1, int[] array2) {
 
         for (int i = 0; i < array1.length; i++) {
@@ -26,6 +28,7 @@ public class BigO {
 
     }
 
+//    O(N^2)
     public void baz(int[] array) {
 
         for (int i = 0; i < array.length; i++) {
@@ -40,13 +43,14 @@ public class BigO {
 
     }
 
+//    O(N^2)
     public void beep(int[] array) {
 
         for (int i = 0; i < array.length; i++) {
             // ...
             for (int j = 0; j < array.length; j++) {
                 // ...
-                for (int k = 0; k < 9999999; k++) {
+                for (int k = 0; k < 9999999; k++) { // O(1)
                     // Jump!
                 }
             }
