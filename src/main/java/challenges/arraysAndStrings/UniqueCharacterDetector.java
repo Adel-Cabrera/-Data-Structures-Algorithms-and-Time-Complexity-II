20dd49cd679c66866128c250341c388088d05e33
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2021-02-20 17:05:29 -0300

    finished introduction to DS & algo's

--- src/main/java/challenges/arraysAndStrings/UniqueCharacterDetector.java
+++ src/main/java/challenges/arraysAndStrings/UniqueCharacterDetector.java
@@ -19,14 +19,22 @@ public class UniqueCharacterDetector {
         if (text.length() > 128) return false;
 
         boolean[] char_set = new boolean[128];
+
         for (int i = 0; i < text.length(); i++) {
             int val = text.charAt(i); // char can be int
+            System.out.println(val);
             if (char_set[val]) { // Already found
                 return false;
             }
             char_set[val] = true;
         }
         return true;
+    }
+    
+    public static void main(String[] args) {
+    	UniqueCharacterDetector ex = new UniqueCharacterDetector();
+    	
+    	System.out.println(ex.isUnique("AaA"));
     }
 
 
